b9e47b478ed3530f5c619e0f4e7cacc9bbd665e2
DL <neoxrock@gmail.com>
2021-12-20 22:44:15 +1100

    middleware to routes

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -5,8 +5,12 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+
+const post = new Router(); // for api/posts/:id
+posts.get('/', postsCtrl.read);
+posts.delete('/', postsCtrl.remove);
+posts.patch('/', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
