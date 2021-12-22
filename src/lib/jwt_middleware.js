abf9b1899fff75cef3b15d3121fafeae588c7076
DL <neoxrock@gmail.com>
2021-12-22 19:09:14 +1100

    reissue token

--- src/lib/jwt_middleware.js
+++ src/lib/jwt_middleware.js
@@ -9,6 +9,15 @@ const jwt_middleware = (ctx, next) => {
       _id: decoded._id,
       username: decoded.username,
     };
+    const now = Math.floor(Date.now() / 1000);
+    if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
+      const user = User.findByUsername(decoded.usename);
+      const token = user.generateToken();
+      ctx.cookies.set('access_token', token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+      });
+    }
     console.log(decoded);
     return next();
   } catch (e) {
