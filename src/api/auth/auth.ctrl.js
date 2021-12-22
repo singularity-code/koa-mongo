67eb08f8fef9b099d4bdb15c5b1943066ff2605f
DL <neoxrock@gmail.com>
2021-12-22 18:10:34 +1100

    Register user

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -1,15 +1,39 @@
-export const register = async ctx => {
+import Joi from 'joi';
+import User from '../../models/user';
 
-}
+export const register = async (ctx) => {
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+  const { username, password } = ctx.request.body;
+  try {
+    const exists = await User.findByUsername(username);
+    if (exists) {
+      ctx.status = 409; // conflict
+      return;
+    }
+    const user = new User({
+      username,
+    });
+    await user.setPassword(password);
+    await user.save();
 
-export const login = async ctx => {
+    // delete passfrom data on response
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
-}
+export const login = async (ctx) => {};
 
-export const check = async ctx => {
+export const check = async (ctx) => {};
 
-}
-
-export const logout = async ctx => {
-  
-}
+export const logout = async (ctx) => {};
