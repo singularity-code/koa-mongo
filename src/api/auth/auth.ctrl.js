22219a8b956d029843494e73b17cc6801aa8b724
DL <neoxrock@gmail.com>
2021-12-22 18:29:09 +1100

    login

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -7,6 +7,7 @@ export const register = async (ctx) => {
     password: Joi.string().required(),
   });
   const result = schema.validate(ctx.request.body);
+  console.log(result);
   if (result.error) {
     ctx.status = 400;
     ctx.body = result.error;
@@ -32,7 +33,28 @@ export const register = async (ctx) => {
   }
 };
 
-export const login = async (ctx) => {};
+export const login = async (ctx) => {
+  const { username, password } = ctx.request.body;
+  if (!username || !password) {
+    ctx.status = 401;
+    return;
+  }
+  try {
+    const user = await User.findByUsername(username);
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 export const check = async (ctx) => {};
 
